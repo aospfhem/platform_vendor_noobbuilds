7709fad4f94b4cd66a618d690cfc105210c7fb87
fwhem <fwhemingway@gmail.com>
2016-06-06 21:15:07 -0400

    Update fingerprints for June 1, 2016 security patch.

--- configs/noob_fingerprints.mk
+++ configs/noob_fingerprints.mk
@@ -3,22 +3,22 @@
 ifeq ($(TARGET_PRODUCT),noobbuilds_n6p)
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=angler \
-    BUILD_FINGERPRINT=google/angler/angler:6.0.1/MTC19T/2741993:user/release-keys \
-    PRIVATE_BUILD_DESC="angler-user 6.0.1 MTC19T 2741993 release-keys"
+    BUILD_FINGERPRINT=google/angler/angler:6.0.1/MTC19V/2862947:user/release-keys \
+    PRIVATE_BUILD_DESC="angler-user 6.0.1 MTC19V 2862947 release-keys"
 endif
 
 ifeq ($(TARGET_PRODUCT),noobbuilds_n6)
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=shamu \
-    BUILD_FINGERPRINT=google/shamu/shamu:6.0.1/MOB30I/2756745:user/release-keys \
-    PRIVATE_BUILD_DESC="shamu-user 6.0.1 MOB30I 2756745 release-keys"
+    BUILD_FINGERPRINT=google/shamu/shamu:6.0.1/MOB30M/2862625:user/release-keys \
+    PRIVATE_BUILD_DESC="shamu-user 6.0.1 MOB30M 2862625 release-keys"
 endif
 
 ifeq ($(TARGET_PRODUCT),noobbuilds_n9)
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=shamu \
-    BUILD_FINGERPRINT=google/shamu/shamu:6.0.1/MOB30G/2723637:user/release-keys \
-    PRIVATE_BUILD_DESC="shamu-user 6.0.1 MOB30G 2723637 release-keys"
+    BUILD_FINGERPRINT=google/shamu/shamu:6.0.1/MOB30M/2862625:user/release-keys \
+    PRIVATE_BUILD_DESC="shamu-user 6.0.1 MOB30M 2862625 release-keys"
 endif
 
        
